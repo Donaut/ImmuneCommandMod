98f27170a92910e599c7e35705caf250c8dbdc4e
McSkinnerOG <mcskinnerleaks@gmail.com>
2020-07-11 04:29:11 +1000

    Update ChatCommandExtensions.cs
    Added 3 guard kits and other random fixes.

--- src/MyMod/ChatCommandExtensions.cs
+++ src/MyMod/ChatCommandExtensions.cs
@@ -124,10 +124,10 @@ namespace MyMod
                         server.CreateFreeWorldItem(170, 1, player.GetPosition(), 100);   // Shoes
                         // Weapons
                         server.CreateFreeWorldItem(95, 1, player.GetPosition(), 100);    // Mutant Claw
-                        server.CreateFreeWorldItem(93, 1, player.GetPosition(), 100);   // C_Knife
+                        server.CreateFreeWorldItem(93, 1, player.GetPosition(), 100);   // Knife
                         server.CreateFreeWorldItem(62, 1, player.GetPosition(), 100);   // SMG
                         // AMMO
-                        server.CreateFreeWorldItem(41, 35, player.GetPosition(), 100);   // 9mm
+                        server.CreateFreeWorldItem(42, 35, player.GetPosition(), 100);   // 556
                         // Food
                         server.CreateFreeWorldItem(12, 2, player.GetPosition(), 100);     // Cooked_fish
                         server.CreateFreeWorldItem(8, 4, player.GetPosition(), 100);     // Energy-Bar
@@ -135,37 +135,153 @@ namespace MyMod
                         server.CreateFreeWorldItem(15, 1, player.GetPosition(), 100);    // Rum-Bottle
                         // ITEMS
                         server.CreateFreeWorldItem(140, 2, player.GetPosition(), 100);   // Bandages
-                        server.CreateFreeWorldItem(143, 2, player.GetPosition(), 100);   // Painkillers
+                        server.CreateFreeWorldItem(142, 2, player.GetPosition(), 100);   // Painkillers
                         server.SendMoneyUpdate(player);
                         server.SendMessageToPlayerLocal("BOUGHT BANDIT KIT!!!", player, msg);
                     }
                     break;
-                case "/weapon":
-                    //Shotgun
-                    server.CreateFreeWorldItem(63, 1, player.GetPosition(), 100);
-                    //Ammo
-                    server.CreateFreeWorldItem(44, 50, player.GetPosition());
-                    //Sneakers
-                    server.CreateFreeWorldItem(170, 1, player.GetPosition(), 100);
+                case "/kit-guard1":
+                    int IA_4 = player.m_inventory.GetItemAmountByType(254);
+                    if (IA_4 <= 4999)
+                    {
+                        server.SendMessageToPlayerLocal("Not Enough Gold!", player, msg);
+                    }
+                    else if (IA_4 >= 5000)
+                    {
+                        int num = 500;
+                        num = Math.Min(IA_4, num);
+                        player.m_inventory.DeclineItemAmountByType(254, num);
+                        // Clothing
+                        server.CreateFreeWorldItem(154, 1, player.GetPosition(), 100);   // Guardian-Vest
+                        server.CreateFreeWorldItem(170, 1, player.GetPosition(), 100);   // Shoes
+                        // Weapons
+                        server.CreateFreeWorldItem(99, 1, player.GetPosition(), 100);    // Giant Sword
+                        server.CreateFreeWorldItem(93, 1, player.GetPosition(), 100);   // Knife
+                        server.CreateFreeWorldItem(65, 1, player.GetPosition(), 100);   // AK47
+                        // AMMO
+                        server.CreateFreeWorldItem(43, 35, player.GetPosition(), 100);   // 762
+                        // Food
+                        server.CreateFreeWorldItem(12, 2, player.GetPosition(), 100);     // Cooked_fish
+                        server.CreateFreeWorldItem(8, 4, player.GetPosition(), 100);     // Energy-Bar
+                        server.CreateFreeWorldItem(16, 1, player.GetPosition(), 100);    // Wine
+                        // ITEMS
+                        server.CreateFreeWorldItem(140, 2, player.GetPosition(), 100);   // Bandages
+                        server.CreateFreeWorldItem(142, 2, player.GetPosition(), 100);   // Painkillers
+                        server.CreateFreeWorldItem(143, 1, player.GetPosition(), 100);   // Medkit
+                        server.SendMoneyUpdate(player);
+                        server.SendMessageToPlayerLocal("BOUGHT GUARD-1 KIT!!!", player, msg);
+                    }
                     break;
-                case "/food":
-                    //Canned Food's
-                    server.CreateFreeWorldItem(10, 2, player.GetPosition());
+                case "/kit-guard2":
+                    int IA_5 = player.m_inventory.GetItemAmountByType(254);
+                    if (IA_5 <= 4999)
+                    {
+                        server.SendMessageToPlayerLocal("Not Enough Gold!", player, msg);
+                    }
+                    else if (IA_5 >= 5000)
+                    {
+                        int num = 500;
+                        num = Math.Min(IA_5, num);
+                        player.m_inventory.DeclineItemAmountByType(254, num);
+                        // Clothing
+                        server.CreateFreeWorldItem(154, 1, player.GetPosition(), 100);   // Guardian-Vest
+                        server.CreateFreeWorldItem(170, 1, player.GetPosition(), 100);   // Shoes
+                        // Weapons
+                        server.CreateFreeWorldItem(99, 1, player.GetPosition(), 100);    // Giant Sword
+                        server.CreateFreeWorldItem(93, 1, player.GetPosition(), 100);   // Knife
+                        server.CreateFreeWorldItem(64, 1, player.GetPosition(), 100);   // Sniper
+                        // AMMO
+                        server.CreateFreeWorldItem(43, 35, player.GetPosition(), 100);   // 762
+                        // Food
+                        server.CreateFreeWorldItem(12, 2, player.GetPosition(), 100);     // Cooked_fish
+                        server.CreateFreeWorldItem(8, 4, player.GetPosition(), 100);     // Energy-Bar
+                        server.CreateFreeWorldItem(16, 1, player.GetPosition(), 100);    // Wine
+                        // ITEMS
+                        server.CreateFreeWorldItem(140, 2, player.GetPosition(), 100);   // Bandages
+                        server.CreateFreeWorldItem(142, 2, player.GetPosition(), 100);   // Painkillers
+                        server.CreateFreeWorldItem(143, 1, player.GetPosition(), 100);   // Medkit
+                        server.SendMoneyUpdate(player);
+                        server.SendMessageToPlayerLocal("BOUGHT GUARD-2 KIT!!!", player, msg);
+                    }
                     break;
-                case "/medicine":
-                    server.CreateFreeWorldItem(143, 2, player.GetPosition());
+                case "/kit-guard3":
+                    int IA_6 = player.m_inventory.GetItemAmountByType(254);
+                    if (IA_6 <= 4999)
+                    {
+                        server.SendMessageToPlayerLocal("Not Enough Gold!", player, msg);
+                    }
+                    else if (IA_6 >= 5000)
+                    {
+                        int num = 500;
+                        num = Math.Min(IA_6, num);
+                        player.m_inventory.DeclineItemAmountByType(254, num);
+                        // Clothing
+                        server.CreateFreeWorldItem(154, 1, player.GetPosition(), 100);   // Guardian-Vest
+                        server.CreateFreeWorldItem(170, 1, player.GetPosition(), 100);   // Shoes
+                        // Weapons
+                        server.CreateFreeWorldItem(99, 1, player.GetPosition(), 100);    // Giant Sword
+                        server.CreateFreeWorldItem(93, 1, player.GetPosition(), 100);   // Knife
+                        server.CreateFreeWorldItem(67, 1, player.GetPosition(), 100);   // Auto-SHotgun
+                        // AMMO
+                        server.CreateFreeWorldItem(44, 35, player.GetPosition(), 100);   // Shells
+                        // Food
+                        server.CreateFreeWorldItem(12, 2, player.GetPosition(), 100);     // Cooked_fish
+                        server.CreateFreeWorldItem(8, 4, player.GetPosition(), 100);     // Energy-Bar
+                        server.CreateFreeWorldItem(16, 1, player.GetPosition(), 100);    // Wine
+                        // ITEMS
+                        server.CreateFreeWorldItem(140, 2, player.GetPosition(), 100);   // Bandages
+                        server.CreateFreeWorldItem(142, 2, player.GetPosition(), 100);   // Painkillers
+                        server.CreateFreeWorldItem(143, 1, player.GetPosition(), 100);   // Medkit
+                        server.SendMoneyUpdate(player);
+                        server.SendMessageToPlayerLocal("BOUGHT GUARD-3 KIT!!!", player, msg);
+                    }
                     break;
                 case "/about":
-                    server.SendMessageUpwards("TEST1", player);
-                    server.SendMessageUpwards("TEST2222", msg);
-                    server.SendMessageToPlayerLocal("Thanks for visiting the server. This is a test server(short-lived) The source code is available at. <color=lime>www.github.com/Donaut/ImmundeCommandMod</color>", player, msg);
+                    server.SendMessageToPlayerLocal("Com-Mod is a project to help expand the Immune-Dedicated software capabilities. For more info please use /commands and /help commandname.", 
+                        player, msg);
                     break;
+                case "/shout":
+                    int IA_C_SHOUT = player.m_inventory.GetItemAmountByType(254); // Start currency Check
+                    if (IA_C_SHOUT <= 499) // Value to check against.
+                    {
+                        server.SendMessageToPlayerLocal("Not Enough Gold!", player, msg); // Return error message if plater des not have enough currency!
+                    }
+                    else if (IA_C_SHOUT >= 500) // If players currency is higher then the value given here
+                    {
+                        string text_shout = msg.ReadString();
+                        server.SendNotification(text_shout);
+                    }  
+                    break;
+
+                case "/commands":
+                    server.SendMessageToPlayerLocal("<color=red>COMMANDS</color> <color=green>ARE</color> <color=purple>COLOR</color> <color=purple>CODED</color>!/nEach command is <color=red>color coded</color> within /help-commandname./nColors represent permission level needed to use them./n<color=red>RED COMMANDS ARE ADMIN ONLY!!!</color>/n<color=yellow>YELLOW COMMANDS REQUIRE GOLD TO EXECUTE!</color>/n<color=green>GREEN COMMANDS ARE ALL LEVEL ACCESS!</color>", player, msg);
+                    break;
+
                 case "/help":
                     server.SendMessageToPlayerLocal("Avalible commands are: <color=green>/kit-doc</color>, <color=purple>/weapon</color>, <color=purple>/food</color>, <color=purple>/medicine</color> and <color=red>/about</color>", player, msg);
                     break;
                 case "/?":
                     server.SendMessageToPlayerLocal("Com-Mod: <color=purple>Made by</color> <color=green>Va1idUser</color> and <color=red>Donaut</color>.", player, msg);
                     break;
+
+ // Useless/Outdated/Wrong method for safe item dispersal/execution. "Potential spam/abuse to cause crashes." Left for educational purposes.
+ /*
+               case "/weapon":
+                     //Shotgun
+                   server.CreateFreeWorldItem(63, 1, player.GetPosition(), 100);
+                     //Ammo
+                   server.CreateFreeWorldItem(44, 50, player.GetPosition());
+                     //Sneakers
+                   server.CreateFreeWorldItem(170, 1, player.GetPosition(), 100);
+                   break;
+               case "/food":
+                     //Canned Food's
+                   server.CreateFreeWorldItem(10, 2, player.GetPosition());
+                    break;
+               case "/medicine":
+                   server.CreateFreeWorldItem(143, 2, player.GetPosition());
+                    break;
+ */
                 default:
                     break;
             }
